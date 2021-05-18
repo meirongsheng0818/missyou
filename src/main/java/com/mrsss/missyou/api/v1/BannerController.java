b6f135e4f2ae410777dad68a6bc7f48bee2051ad
xuguoqiang1 <xuguoqiang1@cnpc.com.cn>
2021-05-18 17:46:43 +0800

    初始化

---
+++ src/main/java/com/mrsss/missyou/api/v1/BannerController.java
@@ -0,0 +1,18 @@
+package com.mrsss.missyou.api.v1;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/banner")
+public class BannerController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "Hello,梅";
+    }
+
+    @PostMapping("/test1")
+    public String test1(){
+        return "Hello World";
+    }
+}
